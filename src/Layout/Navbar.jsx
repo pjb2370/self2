3ee73f918d11bed58ba9cf5b2fb7f714a88d348b
pjb2370 <dbwhd634@naver.com>
2022-12-26 18:33:42 +0900

    aboutme까지

---
+++ src/Layout/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <div id="f" class="Navbar">
+      <div class="Navbar-wrap">
+        <div class="Navbar-image">
+          <img src="https://i.postimg.cc/TYTqFdGZ/image.jpg" alt="" />
+        </div>
+        <div class="Navbar-text">
+          <div class="Navbar-text-main">
+            <Link to="a" spy={true} smooth={true}>
+              <span>About Me</span>
+            </Link>
+            <Link to="b" spy={true} smooth={true}>
+              <span>Skill</span>
+            </Link>
+            <Link to="c" spy={true} smooth={true}>
+              <span>Project</span>
+            </Link>
+          </div>
+          <div class="Navbar-text-table">
+            <div class="Navbar-text-table-row">
+              <div class="Navbar-text-table-cell">
+                <div> - 이름 - </div>
+                <div>웹 프로그래밍 개발자</div>
+
+                <div>안녕하세요.</div>
+                <div>React, MYsql, Java, Spring Boot</div>
+                <div>감사합니다.</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/page/Aboutme.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "../style/Aboutme.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PlaceIcon from "@mui/icons-material/Place";
+import MailIcon from "@mui/icons-material/Mail";
+import CakeIcon from "@mui/icons-material/Cake";
+
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+const Aboutme = () => {
+  return (
+    <div class="Aboutme">
+      <div class="Aboutme_div">
+        <div class="Aboutmetitle">About me</div>
+        <div class="Aboutmeimg">
+          <div class="image">
+            <img
+              class="image1"
+              id="profile-img"
+              src="https://i.postimg.cc/BnJRnf3F/image.png"
+            />
+          </div>
+          <div class="AboutmeMain">
+            <div class="Aboutme1">
+              <div class="Aboutme1div">
+                <span class="Aboutme1icon">
+                  <PersonIcon fontSize="" color="disabled" />
+                </span>
+                <div>
+                  <div>이름</div>
+                  <div>박종범</div>
+                </div>
+              </div>
+
+              <div class="Aboutme1div">
+                <span class="Aboutme1icon">
+                  <CakeIcon fontSize="" color="disabled" />
+                </span>
+                <div>생년월일</div>
+                <div>96.08.01</div>
+              </div>
+            </div>
+            <div class="Aboutme1div">
+              <span class="Aboutme1icon">
+                <PhoneIcon fontSize="" color="disabled" />
+              </span>
+              <div>
+                <div>전화번호</div>
+                <div>010-5095-2370</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="Aboutme1">
+            <div class="Aboutme1div">
+              <span class="Aboutme1icon">
+                <PlaceIcon fontSize="" color="disabled" />
+              </span>
+              <div>
+                <div>주소</div>
+                <div>대전광역시 대덕구 송촌동</div>
+              </div>
+            </div>
+            <div class="Aboutme1div">
+              <span class="Aboutme1icon">
+                <ModeEditIcon fontSize="" color="disabled" />
+              </span>
+              <div>
+                <div>
+                  <div>이메일</div>
+                  <div>dbwhd634@naver.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Aboutme;
